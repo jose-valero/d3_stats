26a325975c9204e8e27c169c608f6bcebfdd3fe0
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-05-03 03:44:03 -0300

    fix

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'ProfileView'
+  name: 'ProfileView',
+  created() {
+    // this.$route.params -> { region: "eu", battleTag: "SuperRambo-2613" }
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      // Llamada API
+    }
+  }
 }
 </script>
