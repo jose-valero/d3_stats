2b91c3006fb6a1ae0b27a0acad822fd3fd2967b5
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-05-04 00:14:37 -0300

    closure profile view

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -3,6 +3,7 @@
     <BaseLoading v-if="isLoading" />
     <template v-if="profileData !== null">
       <MainBlock :profile-data="profileData" />
+      <ArtisansBlock :artisans-data="artisansData" />
     </template>
   </div>
 </template>
@@ -14,11 +15,13 @@ import BaseLoading from '@/components/BaseLoading'
 import setError from '@/mixins/setError'
 import { getApiAccount } from '@/api/search'
 import MainBlock from './MainBlock/Index'
+import ArtisansBlock from './ArtisansBlock/Index'
 
 export default {
   name: 'ProfileView',
   components: {
     BaseLoading,
+    ArtisansBlock,
     MainBlock
   },
   mixins: [setError],
@@ -26,6 +29,18 @@ export default {
     return {
       isLoading: false,
       profileData: null
+    }
+  },
+  computed: {
+    artisansData() {
+      return {
+        blacksmith: this.profileData.blacksmith,
+        blacksmithHardcore: this.profileData.blacksmithHardcore,
+        jeweler: this.profileData.jeweler,
+        jewelerHardcore: this.profileData.jewelerHardcore,
+        mystic: this.profileData.mystic,
+        mysticHardcore: this.profileData.mysticHardcore
+      }
     }
   },
   created() {
